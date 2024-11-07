ef1b5e39b9b504c29d7f5ed647b2a4765c622ecc
Crocobob <mjdaniel911@naver.com>
2024-11-07 13:35:10 +0900

    유도식들 메서드화 성공
    전공책에서 시키는 대로 무지성으로 해봄. 그러니 뭔가 됨.
    여기서 예외처리, 실제 값 연산 추가해야함.

--- Program.cs
+++ Program.cs
@@ -34,6 +34,10 @@ class LexicalAnalyzer {
     private List<string> lexemeList = new List<string>();
     private List<Token> tokenList = new List<Token>();
     private List<String> lineList = new List<string>();
+    private Token currentToken = 0;
+    private Token nextToken = 0;
+    private string currentLexeme = "";
+    private string nextLexeme = "";
 
     private Dictionary<Token, int> tokenCount = new Dictionary<Token, int> {
         { Token.ID, 0 },
@@ -45,43 +49,21 @@ class LexicalAnalyzer {
     private string opSymbols = "(+-*/();)";
     private bool errorFlag = false;
     private List<String> errorList = new List<string>();
-
-    public void PrintAllCounts() {
-        Console.WriteLine("Id count : " + tokenCount[Token.ID] + "Const Count : " + tokenCount[Token.Const] + "\n" +
-                          "Op Count : " + tokenCount[Token.OP]);
-    }
-
-    public void PrintAllToken_debug() {
-        Console.WriteLine("-----------------");
-        Console.WriteLine(" << Print all tokens >>");
-        foreach (var token in tokenList) {
-            Console.WriteLine(token);
-        }
-
-        Console.WriteLine("-----------------");
-    }
-
-    public void PrintAllLexeme_debug() {
-        Console.WriteLine("-----------------");
-        Console.WriteLine(" << Print all lexemes >>");
-        foreach (var lexeme in lexemeList) {
-            Console.WriteLine(lexeme);
-        }
-
-        Console.WriteLine("-----------------");
-    }
-
+    
     public void Analyze(string filePath) {
         MakeStringListFromFile(filePath);
         MakeLexemeList(inputStringList);
+        
+        Statements();
+        if (errorFlag)      PrintStringList(errorList);
 
-        PrintAllCounts();
-        PrintAllLexeme_debug();
-        PrintAllToken_debug();
-
-        // statements(tokenList);
+        // PrintAllCounts_debug();
+        // PrintAllLexeme_debug();
+        // PrintAllToken_debug();
     }
 
+    // 아래는, Text 파일을 읽어와
+    // Lexeme 리스트와 Token 리스트로 변환할 때 사용하는 메소드들
     private void MakeStringListFromFile(string codeFilePath) {
         var readFile = File.ReadAllLines(codeFilePath);
         foreach (var str in readFile) {
@@ -97,18 +79,15 @@ class LexicalAnalyzer {
         // }
         // Console.WriteLine(" ");
     }
-
-
     private void MakeLexemeList(List<string> list) {
         foreach (var lexeme in list) {
             LookUpToken(lexeme);
         }
     }
-
     private void LookUpToken(string str) {
         if (string.IsNullOrEmpty(str)) return;
 
-        Console.WriteLine("Find Lexeme called : " + str);
+        // Console.WriteLine("Find Lexeme called : " + str);
 
         if (char.IsLetter(str[0]) || str[0] == '_') {
             foreach (var c in str) {
@@ -122,62 +101,60 @@ class LexicalAnalyzer {
 
             lexemeList.Add(str);
             tokenList.Add(Token.ID); // Ident라고 판단
-            Console.WriteLine("ID");
+            // Console.WriteLine("ID");
         }
         else if (str.All(char.IsDigit)) {
             // 렉심에 숫자만 있다면
             lexemeList.Add(str);
             tokenList.Add(Token.Const); // 상수로 판단
-            Console.WriteLine("const");
+            // Console.WriteLine("const");
         }
         else {
             // ident와 const 모두 아닌 경우
             LookUpOperatorToken(str); // 특수문자의 토큰을 찾기 위해 lookup 호출
         }
     }
-
     private void LookUpOperatorToken(string str) {
         if (str is ":=") {
             lexemeList.Add(str);
             tokenList.Add(Token.AssignOp);
-            Console.WriteLine("assign op");
+            // Console.WriteLine("assign op");
         }
         else if (str is ";") {
             lexemeList.Add(str);
             tokenList.Add(Token.SemiColon);
-            Console.WriteLine("semi colon");
+            // Console.WriteLine("semi colon");
         }
         else if (str is "+" or "-") {
             lexemeList.Add(str);
             tokenList.Add(Token.AddOp);
-            Console.WriteLine("add op");
+            // Console.WriteLine("add op");
         }
         else if (str is "*" or "/") {
             lexemeList.Add(str);
             tokenList.Add(Token.MultOp);
-            Console.WriteLine("mult op");
+            // Console.WriteLine("mult op");
         }
         else if (str is "(") {
             lexemeList.Add(str);
             tokenList.Add(Token.LeftParen);
-            Console.WriteLine("left paren");
+            // Console.WriteLine("left paren");
         }
         else if (str is ")") {
             lexemeList.Add(str);
             tokenList.Add(Token.RightParen);
-            Console.WriteLine("right paren");
+            // Console.WriteLine("right paren");
         }
         else {
             // 띄어쓰기가 안 되어있거나, 이상한 문자가 입력된 경우
             FixString(str);
         }
     }
-
     private void FixString(string str) {
         var splitStr = new List<String>();
         for (int i = 0; i < str.Length; i++) {
             if (str[i] is '+' or '-' or '*' or '/' or '(' or ')' or ';') {
-                Console.WriteLine(str[i]);
+                //Console.WriteLine(str[i]);
                 splitStr.Add(str[..i]);
                 splitStr.Add(str[i].ToString());
                 splitStr.Add(str[(i + 1)..]);
@@ -203,6 +180,124 @@ class LexicalAnalyzer {
         }
     }
 
+    // 만들어둔 Lexeme 리스트와 Token 리스트에서 다음 요소를 가져와
+    // current와 next token, lexeme을 업데이트하는 함수
+    private void Lexical() {
+        // 택완이형은 이게 있던데...뭘까?
+        //currentToken = nextToken;
+        //currentLexeme = nextLexeme;
+        if (tokenList.Count == 0) {
+            nextToken = 0;
+            nextLexeme = "";
+            return;
+        }
+        
+        nextToken = tokenList[0];
+        nextLexeme = lexemeList[0];
+        
+        tokenList.RemoveAt(0);
+        lexemeList.RemoveAt(0);
+        
+        Console.WriteLine("Next Token = " + nextToken + 
+                          " | Next Lexeme = " + nextLexeme);
+    }
+    
+    private void Statements() {
+        Console.WriteLine("<< Statements enter >>");
+        Statement();
+        do {
+            Lexical();
+            if (nextToken == Token.SemiColon) { 
+                Statement();
+            }
+        }while(nextToken is not 0);
+        
+        Console.WriteLine("<< Statements Exit >>");
+    }
+    
+    private void Statement() {
+        Console.WriteLine("<< Statement enter >>");
+        Lexical();
+        if (nextToken is Token.ID) {
+            Lexical();
+            if (nextToken is Token.AssignOp) {
+                Lexical();
+                Expression();
+            }
+            else {
+                Console.WriteLine("(ERROR) Assign Symbol (:=) is expected.");
+                // errorFlag = true;
+                // errorList.Add("(ERROR) Assign Symbol (:=) is expected.");
+            }
+        }
+        else {
+            Console.WriteLine("(ERROR) Identifier is expected next.");
+            // errorFlag = true;
+            // errorList.Add("(ERROR) Identifier is expected next.");
+        }
+        Console.WriteLine("<< Statement Exit >>");
+    }
+      
+    private void Expression() {
+        Console.WriteLine("<< Expression Enter >>");
+        Term();
+        TermTail();
+    }
+
+    private void TermTail() {
+        Console.WriteLine("<< TermTail Enter >>");
+        if (nextToken is Token.AddOp) {
+            Lexical();
+            Term();
+            TermTail();
+        }
+        else {
+            // 입실론 처리
+        }
+    }
+
+    private void Term() {
+        Console.WriteLine("<< Term Enter >>");
+        Factor();
+        FactorTail();
+    }
+    
+    private void FactorTail() {
+        Console.WriteLine("<< FactorTail Enter >>");
+        if (nextToken is Token.MultOp) {
+            Lexical();
+            Factor();
+            FactorTail();
+        }
+        else {
+            // 입실론 처리 -> 이거 어케 함
+        }
+    }
+  
+    private void Factor() {
+        Console.WriteLine("<< Factor Enter >>");
+        if (nextToken is (Token.ID or Token.Const)) {
+            Lexical();
+        }
+        else {
+            if (nextToken is Token.LeftParen) {
+                Lexical();
+                Expression();
+                if (nextToken is Token.RightParen) {
+                    Lexical();
+                }
+                else {
+                    Console.WriteLine("(ERROR) RightParen expected for next symbol.");
+                    // errorFlag = true;
+                    // errorList.Add("(ERROR) RightParen expected for next symbol.");
+                }
+            }
+        }
+        Console.WriteLine("<< Factor Exit >>");
+    }
+
+    //--------- for Debug. Erase later ------------
+    
     private void PrintStringList(List<String> list) {
         foreach (var str in list) {
             Console.Write(str + ' ');
@@ -210,58 +305,27 @@ class LexicalAnalyzer {
 
         Console.Write("\n");
     }
-
-    private void expectMatch(char current, char expect) {
-        if (current == expect) { }
-        else {
-            errorFlag = true;
-            errorList.Add("(ERROR) " + expect + "가 필요하지만 불필요한 기호 " + current + "가 입력되었습니다.");
+    public void PrintAllCounts_debug() {
+        Console.WriteLine("Id count : " + tokenCount[Token.ID] + "Const Count : " + tokenCount[Token.Const] + "\n" +
+                          "Op Count : " + tokenCount[Token.OP]);
+    }
+    public void PrintAllToken_debug() {
+        Console.WriteLine("-----------------");
+        Console.WriteLine(" << Print all tokens >>");
+        foreach (var token in tokenList) {
+            Console.WriteLine(token);
         }
+
+        Console.WriteLine("-----------------");
+    }
+    public void PrintAllLexeme_debug() {
+        Console.WriteLine("-----------------");
+        Console.WriteLine(" << Print all lexemes >>");
+        foreach (var lexeme in lexemeList) {
+            Console.WriteLine(lexeme);
+        }
+
+        Console.WriteLine("-----------------");
     }
 
-    private void advance() { }
-
-private void Statements(List<Token> tList) {
-        tList = statement(tList);
-        if (tList.Count == 0) {
-            PrintStringList(lineList);
-            Console.Write('\n');
-            Console.WriteLine("ID = " + tokenCount[Token.ID] + ", Const = " + tokenCount[Token.Const] + ", Op = " +
-                              tokenCount[Token.OP]);
-            if (errorFlag) {
-                PrintStringList(errorList);
-            }
-            else {
-                Console.WriteLine("(OK)");
-            }
-
-            Console.Write("Result ==> ");
-            foreach (var pair in idResult) {
-                Console.Write("{0}: {1}", pair.Key, pair.Value);
-            }
-
-            Console.Write("\n");
-        }
-        else if (tList[0] == Token.SemiColon) {
-            if (tList[1] is not (Token.ID or Token.Const)) {
-                errorFlag = true;
-                errorList.Add("(Warning) 잘못된 기호 " + tList[1] + " 제거");
-                tList.RemoveAt(1);
-                lexemeList.RemoveAt(1);
-            }
-
-            lineList.Add(lexemeList[0]);
-            tList.RemoveAt(0);
-            lineList.RemoveAt(0);
-        }
-    }
-    
-    List<Token> statement(List<Token> tList) {
-        if (tList[0] == Token.ID) {
-            tList.RemoveAt(0);
-            tokenCount[Token.ID]++;
-        }
-
-        return null;
-    }
 }
